ecc3db541f3cd8147e5d6120e14cda834c747eed
Suzannah <suzannah.herron@gmail.com>
2022-01-13 12:41:49 -0700

    comment and aesthetic updates

--- make_cache_matrix.R
+++ make_cache_matrix.R
@@ -1,29 +1,34 @@
-make_cache_matrix <- function(x = matrix())
+# I use `_` in my functions for readability and to catch typos
+
+make_cache_matrix <- function(x = matrix()) 
 {
-  inv <- NULL
-  set <- function(y)
-  {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() {x}
-  set_inv <- function(inverse) 
-  {inv <<- inverse}
-  get_inv <- function() 
-  {inv}
-  list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
+    inv <- NULL
+    set <- function(y)
+    {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() 
+        {x}                         
+    set_inv <- function(inverse)    
+        {inv <<- inverse}               
+    get_inv <- function()           
+        {inv}
+    list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
 }
+
+
 
 cache_solve <- function(x, ...)
 {
-  inv <- x$get_inv()
-  if(!is.null(inv))
-  {
-    message("getting cached data")
-    return(inv)
-  }
-  mtx <- x$get()
-  inv <- solve(mtx, ...)
-  x$set_inv
-  inv
+    inv <- x$get_inv()
+    if(!is.null(inv))  # this skips calculation of `inv` has already been calculated
+    {
+        message("getting cached data")
+        return(inv)
+    }
+    mtx <- x$get()
+    inv <- solve(mtx, ...)          #calc inverse using `solve()`
+    x$set_inv
+    inv
 }
